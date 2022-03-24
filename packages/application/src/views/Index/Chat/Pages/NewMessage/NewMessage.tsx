5aa612f5cfdd28bd37b4b08f576b6648e06c52ea
Shuanglin Deng <2447749681@qq.com>
2022-03-24 21:27:04 +0800

    fix: 优化输入框逻辑

--- packages/application/src/views/Index/Chat/Pages/NewMessage/NewMessage.tsx
+++ packages/application/src/views/Index/Chat/Pages/NewMessage/NewMessage.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import './NewMessage.css'
 import '../../Style/index.css'
-import ChatHeader from "../../Components/ChatHeader/ChatHeader"
+import ChatHeader from '../../Components/ChatHeader/ChatHeader'
 import ChatBody from '../../Components/ChatBody/ChatBody'
-import {MarkdownInputEditor} from "@slack-pkg/components";
+import { MarkdownInputEditor } from '@slack-pkg/components'
 
 export default function NewMessage() {
   return (
     <div className='main_chat_contents'>
-      <ChatHeader title="新消息" />
-      <ChatBody/>
+      <ChatHeader title='新消息' />
+      <ChatBody />
 
-        <MarkdownInputEditor style={{flex:'162px',width:'100%'}}/>
+      <MarkdownInputEditor style={{ flex: '162px' }} />
     </div>
   )
 }
